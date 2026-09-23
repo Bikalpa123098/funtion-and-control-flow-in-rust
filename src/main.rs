340084290cc94cea1fd8031bd72693305d4a3b7d
Bikalpa123098 <koiralabikalpa665@gmail.com>
2026-09-23 09:53:20 +0545

    function with parameters

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+    another_function();
+    another_function_with_parameters(5);
+}
+
+fn another_function(){
+    println!("This is an another function!");
+}
+
+fn another_function_with_parameters(x:i32) {
+    println!("The value of x is: {}",x);
 }
