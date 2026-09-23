b1607187c0a151bca29e5202b952c490f16101de
Bikalpa123098 <koiralabikalpa665@gmail.com>
2026-09-23 12:45:17 +0545

    statement_and_expression

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     another_function();
     another_function_with_parameters(5);
     another_function_with_multiple_parameters("Bikalpa", 20);
+    statement_and_expression();
 }
 
 fn another_function(){
@@ -15,4 +16,11 @@ fn another_function_with_parameters(x:i32) {
 
 fn another_function_with_multiple_parameters(name:&str, age:u8){
     println!("My name is {} and I am {} years old!",name,age);
+}
+fn statement_and_expression(){
+    let x = {
+        let y = 5;
+        y+6
+    };
+    println!("{}",x);
 }
